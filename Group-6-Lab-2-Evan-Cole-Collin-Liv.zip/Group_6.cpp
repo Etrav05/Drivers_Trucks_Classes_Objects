490be72efaf64851502240c8edf018c1d2ca9d57
Evan Travis <Etravis9851@conestogac.on.ca>
2025-02-01 12:40:38 -0500

    Re-evaulated the classes which were made, now there are 3 classes: Driver, Location and Truck Also created a function within the truck class to find the distance between 2 trucks or the origin (0, 0) -> may need to be updated though

--- Group-6-Lab-2-Evan-Cole-Collin-Liv.zip/Group_6.cpp
+++ Group-6-Lab-2-Evan-Cole-Collin-Liv.zip/Group_6.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <math.h>
 
 using namespace std;
 
@@ -7,37 +8,55 @@ struct driver {  // driver class
 	string firstName;
 	string lastName;
 	int driverId;
+
+public:
+	driver createDriver(string firstName, string lastName, int id) {  // function to create driver object
+		driver d;
+		d.firstName = firstName;
+		d.lastName = lastName;
+		d.driverId = id;
+		return d;
+	}
 };
 
-struct truck {  // truck class
-	int truckId;
+struct location {  // location class
+	int x;
+	int y;
 
-	int locationX;
-	int locationY;
+public:
+	location createLocation(int x, int y) { // function to create location 
+		location l;
+		l.x = x;
+		l.y = y;
+		return l;
+	}
 };
 
-driver createDriver(string firstName, string lastName, int id) {  // function to create driver object
+struct truck {  // truck class -> with driver and location attributes 
 	driver d;
-	d.firstName = firstName;
-	d.lastName = lastName;
-	d.driverId = id;
-	return d;
-}
+	location l;
 
-truck createTruck(int id, int x, int y) { // function to create truck object
-	truck t;
-	t.truckId = id;
-	t.locationX = x;
-	t.locationY = y;
-	return t;
-}
+public: 
+
+	double distanceFrom(int x, int y) {
+		int location1 = pow(2, (l.x - l.y));  // loctation of given truck
+		int location2 = pow(2, (x - y));  // location of origin or other truck
+
+		return sqrt(location1 + location2);
+	}
+};
+
 
 int main() {
-	
-	driver d1, d2, d3;  // initialize and set drivers values 
-	d1 = createDriver("Smith", "John", 1001);
-	d2 = createDriver("Travis", "Evan", 1002);
-	d3 = createDriver("Glump", "Forest", 1003);
+	driver d1, d2, d3;  // initialize driver and truck objects
+	truck t1, t2;
 
+	d1.createDriver("Smith", "John", 1001);  // create driver objects
+	d2.createDriver("Travis", "Evan", 1002);
+	d3.createDriver("Glump", "Forest", 1003);
+
+
+
+	return 0;
 }
 
